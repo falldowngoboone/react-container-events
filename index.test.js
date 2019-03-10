30e303fa67291801317ac696afc2d0e81ba13245
Ryan Boone <falldowngoboone@gmail.com>
2019-03-10 13:04:05 -0500

    Add happy path test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,21 @@
-import { renderWithContainerEvents, useContainerEvent } from "./index.js";
+import React from 'react';
+import { renderWithContainerEvents, useContainerEvent } from './index.js';
 
-describe("renderWithContainerEvents", () => {
-  it("works", () => {
-    expect(true).toBe(true);
-  });
+test('Custom events dispatched on container trigger useContainerEvent listeners', () => {
+  const container = document.createElement('div');
+  const spy = jest.fn();
+  document.body.appendChild(container);
+
+  renderWithContainerEvents(<Component />, container);
+  // force a rerender since useEffect is asyncrhonous to prevent render blocking
+  renderWithContainerEvents(<Component />, container);
+
+  container.dispatchEvent(new CustomEvent('test'));
+
+  expect(spy).toHaveBeenCalled();
+
+  function Component() {
+    useContainerEvent('test', spy);
+    return <div />;
+  }
 });
